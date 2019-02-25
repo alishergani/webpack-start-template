a2701f42f4e0c9706e0df665b4c2cb65a0a89d0c
alishergani <ka4anbek@gmail.com>
2019-02-25 12:57:53 +0600

    template done!

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+// import jQuery from "jquery";
+// import popper from "popper.js";
+// import bootstrap from "bootstrap";
+import vue from "vue/dist/vue.min.js";
+
+const app = new vue({
+	el: '#app',
+	data() {
+		return {
+			title: "Hello, Vue!"
+		}
+	}
+})
